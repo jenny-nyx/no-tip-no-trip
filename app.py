684bce2a0f4dff3ec5740b3eaaf2bc26723eadcf
jenny <jenthakkar@gmail.com>
2023-04-29 17:27:50 -0400

    stuff and things

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 import calculator
 
-app = Flask("main")
-
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def root():
+    # this will probably take in miles and return trip?
     calculator.main()
     return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run( debug = True)
--- calculator.py
+++ calculator.py
@@ -1,4 +1,28 @@
 import requests
 
+def calculator( miles, returnTrip, costOfOwnership, gas, orderPrice ):
+    '''
+    '''
+    totalMilesPerYear = 50000
+
+    costPerMile = costOfOwnership / totalMilesPerYear
+
+    coverCostNoReturn = miles * costPerMile
+    coverCostWithReturn = ( miles * 2 ) * costPerMile
+    profitNoReturn = coverCostNoReturn - orderPrice
+    profitWithReturn = coverCostWithReturn - orderPrice
+
+
+    retVal = [coverCostNoReturn, coverCostWithReturn, profitNoReturn, profitWithReturn]
+    
+    return retVal
+
 def main():
-    print("hello")
+    miles = 7
+    returnTrip = False
+    costOfOwnership = 31907
+    gasPerG = 3.25
+    orderPrice = 10
+    result = calculator( miles, returnTrip, costOfOwnership, gasPerG, orderPrice )
+    print (result)
+    return result
